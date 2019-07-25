b1a63b0e14df2ee54d78f78860d7da6b5b3fc97a
wbsong <51451584+oneofSong@users.noreply.github.com>
2019-07-25 18:05:04 +0900

    update data_augmentation_for_yolo

--- data_aug_for_yolo.py
+++ data_aug_for_yolo.py
@@ -3,7 +3,6 @@ import cv2
 import random
 import copy
 
-
 def RandomFlip(img,bboxes,mode=1,p=1.0):
     # mode : 0 상하 반전 1 좌우 반전
     src = copy.deepcopy(img)
@@ -11,7 +10,7 @@ def RandomFlip(img,bboxes,mode=1,p=1.0):
 
     if random.random() < p:
         print("flip 실행")
-        dst = cv2.flip(img,mode)
+        dst = cv2.flip(src, mode)
 
         for i in range(len(bbox)):
             if mode == 0:
@@ -29,13 +28,14 @@ def RandomResize(img, bbox, scale=0.7, p=1.0):
     src = copy.deepcopy(img)
     bbox = copy.deepcopy(bbox)
 
-    if random.random() < p:
+    if random.random() >= p:
         return src,bbox
 
     dsize = (int(src.shape[1] * scale),int(src.shape[0] * scale))
     dst = np.zeros(src.shape)
 
     src = cv2.resize(src,dsize,interpolation=cv2.INTER_AREA)
+    # print(src.shape)
 
     for i in range(src.shape[0]):
         for j in range(src.shape[1]):
@@ -43,14 +43,13 @@ def RandomResize(img, bbox, scale=0.7, p=1.0):
 
     for i in range(len(bbox)):
         for j in range(5):
-            if i != 0:
+            if j != 0:
                 bbox[i][j] = bbox[i][j] * scale
-
     return dst,bbox
 
 
-def RandomRotate(img,angle,p=1.0):
-    if random.random() < p:
+def RandomRotate(img,angle,scale=1.0,p=1.0):
+    if random.random() > p:
         print("rotate 실행")
         return img
 
@@ -62,12 +61,14 @@ def RandomRotate(img,angle,p=1.0):
     return rotated
 
 
-def SaveImage(img,bbox,save_path,name):
-    cv2.imwrite(save_path + name + ".jpg",img)
 
-    with open(name + ".txt",'w') as f:
-        for i in range(len(bbox)):
-            for j in range(5):
-                f.write(str(bbox[i][j]) + " ")
+if __name__ == "__main__":
+    img = cv2.imread("lena.jpg")
 
-            f.write("\n")
+    cv2.imshow("img1", img)
+
+    img2, _ = RandomResize(img, [])
+
+    cv2.imshow("img2", img2)
+    cv2.imwrite("lena_resize.jpg", img2)
+    cv2.waitKey(0)
