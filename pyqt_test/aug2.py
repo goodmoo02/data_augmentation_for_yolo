2bb8254448d89b7d9fbc9f15640c1eed07e70c48
goodmoo02 <goodmoo02@gmail.com>
2019-07-31 14:03:17 +0900

    update

--- pyqt_test/aug2.py
+++ pyqt_test/aug2.py
@@ -22,17 +22,17 @@ class Ui_MainWindow(object):
         self.v_flip_flag = -1
         self.pgbar_num = 0
 
-        MainWindow.setObjectName("MainWindow")
+        # MainWindow.setObjectName("MainWindow")
         MainWindow.resize(693, 450)
         self.centralwidget = QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
+        # self.centralwidget.setObjectName("centralwidget")
 
         self.horizontalLayoutWidget = QWidget(self.centralwidget)
         self.horizontalLayoutWidget.setGeometry(QRect(20, 10, 651, 51))
-        self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
+        # self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
         self.horizontalLayout = QHBoxLayout(self.horizontalLayoutWidget)
         self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
-        self.horizontalLayout.setObjectName("horizontalLayout")
+        # self.horizontalLayout.setObjectName("horizontalLayout")
 
         self.dir_label = QLabel(self.horizontalLayoutWidget)
         sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
@@ -42,171 +42,171 @@ class Ui_MainWindow(object):
 
         self.dir_label.setSizePolicy(sizePolicy)
         self.dir_label.setBaseSize(QSize(0, 0))
-        self.dir_label.setObjectName("dir_label")
+        # self.dir_label.setObjectName("dir_label")
         self.horizontalLayout.addWidget(self.dir_label)
 
         self.dir_line = QLineEdit(self.horizontalLayoutWidget)
-        self.dir_line.setObjectName("dir_line")
+        # self.dir_line.setObjectName("dir_line")
         self.horizontalLayout.addWidget(self.dir_line)
 
         self.dir_warn_label = QLabel(self.centralwidget)
         self.dir_warn_label.setGeometry(QRect(95, 49, 241, 31))
         self.dir_warn_label.setText("")
-        self.dir_warn_label.setObjectName("dir_warn_label")
+        # self.dir_warn_label.setObjectName("dir_warn_label")
 
         self.find_btn = QPushButton(self.horizontalLayoutWidget)
-        self.find_btn.setObjectName("find_btn")
+        # self.find_btn.setObjectName("find_btn")
         self.horizontalLayout.addWidget(self.find_btn)
 
         self.option_box = QGroupBox(self.centralwidget)
         self.option_box.setGeometry(QRect(20, 80, 651, 341))
-        self.option_box.setObjectName("option_box")
+        # self.option_box.setObjectName("option_box")
 
         self.resize_label = QLabel(self.option_box)
         self.resize_label.setGeometry(QRect(67, 40, 41, 21))
         self.resize_label.setLayoutDirection(Qt.LeftToRight)
-        self.resize_label.setObjectName("resize_label")
+        # self.resize_label.setObjectName("resize_label")
 
         self.x_size = QLabel(self.option_box)
         self.x_size.setGeometry(QRect(67, 70, 41, 21))
         self.x_size.setLayoutDirection(Qt.LeftToRight)
-        self.x_size.setObjectName("x_size")
+        # self.x_size.setObjectName("x_size")
 
         self.x_line = QSpinBox(self.option_box)
         self.x_line.setGeometry(QRect(110, 70, 50, 20))
-        self.x_line.setObjectName("x_line")
+        # self.x_line.setObjectName("x_line")
         self.x_line.setRange(0, 2000)
         self.x_line.setSingleStep(1)
 
         self.y_size = QLabel(self.option_box)
         self.y_size.setGeometry(QRect(167, 70, 41, 21))
         self.y_size.setLayoutDirection(Qt.LeftToRight)
-        self.y_size.setObjectName("y_size")
+        # self.y_size.setObjectName("y_size")
 
         self.y_line = QSpinBox(self.option_box)
         self.y_line.setGeometry(QRect(210, 70, 50, 20))
-        self.y_line.setObjectName("y_line")
+        # self.y_line.setObjectName("y_line")
         self.y_line.setRange(0, 2000)
         self.y_line.setSingleStep(1)
 
         self.explain_size = QLabel(self.option_box)
         self.explain_size.setGeometry(QRect(267, 70, 280, 21))
         self.explain_size.setLayoutDirection(Qt.LeftToRight)
-        self.explain_size.setObjectName("explain_size")
+        # self.explain_size.setObjectName("explain_size")
 
         self.resize_line = QDoubleSpinBox(self.option_box)
         self.resize_line.setGeometry(QRect(110, 40, 38, 20))
-        self.resize_line.setObjectName("resize_line")
+        # self.resize_line.setObjectName("resize_line")
         self.resize_line.setRange(0, 4)
         self.resize_line.setSingleStep(0.1)
 
 
         self.rotate_label = QLabel(self.option_box)
         self.rotate_label.setGeometry(QRect(294, 40, 41, 20))
-        self.rotate_label.setObjectName("rotate_label")
+        # self.rotate_label.setObjectName("rotate_label")
 
         self.rotate_line = QSpinBox(self.option_box)
         self.rotate_line.setGeometry(QRect(337, 40, 45, 20))
-        self.rotate_line.setObjectName("rotate_line")
+        # self.rotate_line.setObjectName("rotate_line")
         self.rotate_line.setRange(0, 360)
         self.rotate_line.setSingleStep(1)
 
         self.degree_label = QLabel(self.option_box)
         self.degree_label.setGeometry(QRect(377, 40, 91, 20))
-        self.degree_label.setObjectName("degree_label")
+        # self.degree_label.setObjectName("degree_label")
 
         self.flip_label = QLabel(self.option_box)
         self.flip_label.setGeometry(QRect(22, 143, 31, 31))
-        self.flip_label.setObjectName("flip_label")
+        # self.flip_label.setObjectName("flip_label")
 
         self.v_flip_label = QLabel(self.option_box)
         self.v_flip_label.setGeometry(QRect(71, 143, 31, 31))
-        self.v_flip_label.setObjectName("v_flip_label")
+        # self.v_flip_label.setObjectName("v_flip_label")
 
         self.v_flip_cb = QCheckBox(self.option_box)
         self.v_flip_cb.setGeometry(QRect(117, 148, 16, 21))
         self.v_flip_cb.setText("")
-        self.v_flip_cb.setObjectName("v_flip_cb")
+        # self.v_flip_cb.setObjectName("v_flip_cb")
 
         self.h_flip_label = QLabel(self.option_box)
         self.h_flip_label.setGeometry(QRect(155, 143, 31, 31))
-        self.h_flip_label.setObjectName("h_flip_label")
+        # self.h_flip_label.setObjectName("h_flip_label")
 
         self.h_flip_cb = QCheckBox(self.option_box)
         self.h_flip_cb.setGeometry(QRect(203, 148, 16, 21))
         self.h_flip_cb.setText("")
-        self.h_flip_cb.setObjectName("h_flip_cb")
+        # self.h_flip_cb.setObjectName("h_flip_cb")
 
         self.rrs_label = QLabel(self.option_box)
         self.rrs_label.setGeometry(QRect(20, 35, 31, 31))
-        self.rrs_label.setObjectName("rrs_label")
+        # self.rrs_label.setObjectName("rrs_label")
 
         self.pgbar = QProgressBar(self.option_box)
         self.pgbar.setGeometry(QRect(70, 303, 450, 20))
-        self.pgbar.setObjectName("pgbar")
+        # self.pgbar.setObjectName("pgbar")
         self.pgbar.setMaximum(100)
 
         self.start_btn = QPushButton(self.option_box)
         self.start_btn.setGeometry(QRect(530, 301, 70, 23))
-        self.start_btn.setObjectName("start_btn")
+        # self.start_btn.setObjectName("start_btn")
 
         self.pg_label = QLabel(self.option_box)
         self.pg_label.setGeometry(QRect(10, 303, 61, 20))
-        self.pg_label.setObjectName("pg_label")
+        # self.pg_label.setObjectName("pg_label")
 
         self.rs_times_label = QLabel(self.option_box)
         self.rs_times_label.setGeometry(QRect(150, 40, 111, 20))
-        self.rs_times_label.setObjectName("rs_times_label")
+        # self.rs_times_label.setObjectName("rs_times_label")
 
         self.scale_label = QLabel(self.option_box)
         self.scale_label.setGeometry(QRect(70, 110, 41, 21))
-        self.scale_label.setObjectName("scale_label")
+        # self.scale_label.setObjectName("scale_label")
 
         self.scale_line = QDoubleSpinBox(self.option_box)
         self.scale_line.setGeometry(QRect(110, 110, 38, 20))
-        self.scale_line.setObjectName("scale_line")
+        # self.scale_line.setObjectName("scale_line")
         self.scale_line.setRange(0, 1)
         self.scale_line.setSingleStep(0.1)
 
         self.sc_times_label = QLabel(self.option_box)
         self.sc_times_label.setGeometry(QRect(150, 110, 111, 20))
-        self.sc_times_label.setObjectName("sc_times_label")
+        # self.sc_times_label.setObjectName("sc_times_label")
 
         self.rename_label = QLabel(self.option_box)
         self.rename_label.setGeometry(QRect(11, 183, 50, 31))
-        self.rename_label.setObjectName("rename_label")
+        # self.rename_label.setObjectName("rename_label")
 
         self.rename_exp_label = QLabel(self.option_box)
         self.rename_exp_label.setGeometry(QRect(80, 210, 50, 31))
-        self.rename_exp_label.setObjectName("rename_exp_label")
+        # self.rename_exp_label.setObjectName("rename_exp_label")
 
         self.all_rb = QRadioButton(self.option_box)
         self.all_rb.setGeometry(QRect(80, 190, 90, 16))
-        self.all_rb.setObjectName("all_rb")
+        # self.all_rb.setObjectName("all_rb")
         self.all_rb.setChecked(True)
 
         self.what_rb = QRadioButton(self.option_box)
         self.what_rb.setGeometry(QRect(280, 190, 111, 16))
-        self.what_rb.setObjectName("what_rb")
+        # self.what_rb.setObjectName("what_rb")
 
         self.no_rb = QRadioButton(self.option_box)
         self.no_rb.setGeometry(QRect(180, 190, 90, 16))
-        self.no_rb.setObjectName("no_rb")
+        # self.no_rb.setObjectName("no_rb")
 
         self.what_line = QLineEdit(self.option_box)
         self.what_line.setGeometry(QRect(397, 188, 71, 20))
-        self.what_line.setObjectName("what_line")
+        # self.what_line.setObjectName("what_line")
         self.what_line.setEnabled(False)
 
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setGeometry(QRect(0, 0, 736, 21))
-        self.menubar.setObjectName("menubar")
+        # self.menubar.setObjectName("menubar")
         self.menuImage_Data_Augmentation = QMenu(self.menubar)
-        self.menuImage_Data_Augmentation.setObjectName("menuImage_Data_Augmentation")
+        # self.menuImage_Data_Augmentation.setObjectName("menuImage_Data_Augmentation")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
+        # self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
@@ -215,37 +215,37 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "IDA(Image Data Augmentation)"))
-        self.dir_label.setText(_translate("MainWindow", "Directory"))
-        self.dir_line.setPlaceholderText(_translate("MainWindow", "Put image path"))
-        self.find_btn.setText(_translate("MainWindow", "&Find"))
-        self.option_box.setTitle(_translate("MainWindow", "Option"))
-        self.resize_label.setText(_translate("MainWindow", "Resize"))
-        self.x_size.setText(_translate("MainWindow", "X_size"))
-        self.y_size.setText(_translate("MainWindow", "Y_size"))
-        self.explain_size.setText(_translate("MainWindow", "px  (When Resize is 1, You can use X, Y size.)"))
+        MainWindow.setWindowTitle("IDA(Image Data Augmentation)")
+        self.dir_label.setText("Directory")
+        self.dir_line.setPlaceholderText("Put image path")
+        self.find_btn.setText("&Find")
+        self.option_box.setTitle("Option")
+        self.resize_label.setText("Resize")
+        self.x_size.setText("X_size")
+        self.y_size.setText("Y_size")
+        self.explain_size.setText("px  (When Resize is 1, You can use X, Y size.)")
         self.resize_line.setValue(1)
         self.resize_line.setDecimals(1)
-        self.rotate_label.setText(_translate("MainWindow", "Rotate"))
+        self.rotate_label.setText("Rotate")
         self.rotate_line.setValue(0)
-        self.degree_label.setText(_translate("MainWindow", "  °(도) (Default)"))
-        self.flip_label.setText(_translate("MainWindow", "Flip"))
-        self.v_flip_label.setText(_translate("MainWindow", "v_flip"))
-        self.h_flip_label.setText(_translate("MainWindow", "h_flip"))
-        self.rrs_label.setText(_translate("MainWindow", "RRS"))
-        self.start_btn.setText(_translate("MainWindow", "&Start"))
-        # self.stop_btn.setText(_translate("MainWindow", "&Stop"))
-        self.pg_label.setText(_translate("MainWindow", "Progress"))
-        self.rs_times_label.setText(_translate("MainWindow", "times(배) (Default)"))
-        self.scale_label.setText(_translate("MainWindow", "Scale"))
+        self.degree_label.setText("  °(도) (Default)")
+        self.flip_label.setText("Flip")
+        self.v_flip_label.setText("v_flip")
+        self.h_flip_label.setText("h_flip")
+        self.rrs_label.setText("RRS")
+        self.start_btn.setText("&Start")
+        # self.stop_btn.setText("&Stop")
+        self.pg_label.setText("Progress")
+        self.rs_times_label.setText("times(배) (Default)")
+        self.scale_label.setText("Scale")
         self.scale_line.setValue(1)
         self.scale_line.setDecimals(1)
-        self.sc_times_label.setText(_translate("MainWindow", "times(배) (Default)"))
-        self.rename_label.setText(_translate("MainWindow", "Rename"))
-        self.rename_exp_label.setText(_translate("MainWindow", "all"))
-        self.all_rb.setText(_translate("MainWindow", " &All"))
-        self.what_rb.setText(_translate("MainWindow", " &What you want"))
-        self.no_rb.setText(_translate("MainWindow", " &No"))
+        self.sc_times_label.setText("times(배) (Default)")
+        self.rename_label.setText("Rename")
+        self.rename_exp_label.setText("all")
+        self.all_rb.setText(" &All")
+        self.what_rb.setText(" &What you want")
+        self.no_rb.setText(" &No")
 
     def utilUi(self, MainWindow):
         self.dir_line.textChanged[str].connect(self.dir_line_changed)
@@ -263,7 +263,7 @@ class Ui_MainWindow(object):
         self.start_btn.setEnabled(True)
 
     def dir_btn_clicked(self):
-        fname = QFileDialog.getExistingDirectory(None, 'Select a folder:', 'C:/Users/bit/Desktop/yolo/img3', QFileDialog.ShowDirsOnly)
+        fname = QFileDialog.getExistingDirectory(None, 'Select a folder:', 'C:/Users/bit/Desktop', QFileDialog.ShowDirsOnly)
         self.dir_line.setText(fname)
 
     def resize_line_changed(self):
@@ -357,15 +357,15 @@ class Ui_MainWindow(object):
 
                 self.flag = False
 
-def exception_hook(exctype, value, traceback):
-    print(exctype, value, traceback)
-    sys._excepthook(exctype, value, traceback)
-    sys.exit(1)
+# def exception_hook(exctype, value, traceback):
+#     print(exctype, value, traceback)
+#     sys._excepthook(exctype, value, traceback)
+#     sys.exit(1)
 
 if __name__ == "__main__":
     import sys
 
-    sys.excepthook = exception_hook
+    # sys.excepthook = exception_hook
     app = QApplication(sys.argv)
     MainWindow = QMainWindow()
     ui = Ui_MainWindow()
